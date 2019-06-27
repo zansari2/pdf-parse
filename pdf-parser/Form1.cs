c5364b2220c147503059e7dfe8dd99e51b9ef05b
Zishan Ansari <42755217+zansari2@users.noreply.github.com>
2019-06-27 11:25:06 -0400

    Commenting classes

--- pdf-parser/Form1.cs
+++ pdf-parser/Form1.cs
@@ -1,31 +1,60 @@
-﻿using System;
+﻿//Import statements
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Package information
 namespace pdf_parser
 {
+    //Class declaration
     public partial class ApplicationForm : Form
     {
+        //Sql adapter and binding source for changing tables
+        private SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+        private BindingSource bindingSource = new BindingSource();
+        //Default constructor, initializes components
         public ApplicationForm()
         {
             InitializeComponent();
         }
-
+        //Load method
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'schoolDataSet.Assignments' table. You can move, or remove it, as needed.
-            this.assignmentsTableAdapter.Fill(this.schoolDataSet.Assignments);
+            dataGridView1.DataSource = bindingSource;
+            SetDataGrid("Select * From Assignments");
         }
-
+        //Click method
         private void Swap_Click(object sender, EventArgs e)
         {
-           
+            if(dataGridView1.Columns[0].Name=="Date")
+            {
+                SetDataGrid("Select * From Emails");
+            }
+            else
+            {
+                SetDataGrid("Select * From Assignments");
+            }
+        }
+        //Swap data method
+        private void SetDataGrid(string selectCommand)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\zansari\Documents\GitHub\pdf-parse\pdf-parser\School.mdf;Integrated Security=True";
+
+            sqlDataAdapter = new SqlDataAdapter(selectCommand, connectionString);
+
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+
+            DataTable table = new DataTable { Locale = CultureInfo.InvariantCulture };
+
+            sqlDataAdapter.Fill(table);
+            bindingSource.DataSource = table;
         }
     }
 }
--- pdf-parser/Program.cs
+++ pdf-parser/Program.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿//Import statements
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Package information
 namespace pdf_parser
 {
+    //Class declaration
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        //Main method
         [STAThread]
         static void Main()
         {
+            //Calls application method
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Runs a new application form
             Application.Run(new ApplicationForm());
         }
     }
